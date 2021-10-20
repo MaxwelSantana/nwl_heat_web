86d467c90d5a3e197c9b844258b5c6b7b1c9ceb5
Maxwel <maxwelsant96@icloud.com>
2021-10-19 21:59:36 -0300

    getting new messages with socket io

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg';
 import { api } from '../../services/api';
+import { io } from 'socket.io-client';
 
 type Message = {
   id: string;
@@ -13,8 +14,29 @@ type Message = {
   };
 };
 
+const messageQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messageQueue.push(newMessage);
+});
+
 const MessageList = () => {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messageQueue.length > 0) {
+        setMessages((prevMessages) =>
+          [messageQueue[0], prevMessages[0], prevMessages[1]].filter(Boolean),
+        );
+
+        messageQueue.shift();
+      }
+    }, 3000);
+  }, []);
+
   useEffect(() => {
     api.get<Message[]>('messages/last3').then((response) => {
       setMessages(response.data);
