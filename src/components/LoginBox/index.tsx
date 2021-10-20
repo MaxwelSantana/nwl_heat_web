19f18864b841cd63e6fc2c9dbc9f1ece32b923c2
Maxwel <maxwelsant96@icloud.com>
2021-10-19 21:19:21 -0300

    added auth context api

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,43 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc';
 import { api } from '../../services/api';
-
-type AuthResponse = {
-  token: string;
-  user: {
-    id: string;
-    avatar_url: string;
-    name: string;
-    login: string;
-  };
-};
+import { AuthContext } from '../../contexts/auth';
 
 const LoginBox = () => {
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=768290e6be094c662f7d`;
+  const { signInUrl, user } = useContext(AuthContext);
 
-  async function signIn(githubCode: string) {
-    const response = await api.post<AuthResponse>('authenticate', {
-      code: githubCode,
-    });
-
-    const { token, user } = response.data;
-
-    localStorage.setItem('@dowile:token', token);
-    console.log(user);
-  }
-
-  useEffect(() => {
-    const url = window.location.href;
-    const hasGithubCode = url.includes('?code=');
-
-    if (hasGithubCode) {
-      const [urlWithoutCode, githubCode] = url.split('?code=');
-      window.history.pushState({}, '', urlWithoutCode);
-
-      signIn(githubCode);
-    }
-  }, []);
+  console.log(user);
 
   return (
     <div className={styles.loginBoxWrapper}>
